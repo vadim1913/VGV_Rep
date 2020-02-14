8cab8a0fa245ad5f5e51bc3ad6a388724c9d2849
Vadim <60069107+vadim1913@users.noreply.github.com>
2020-02-13 19:22:46 -0500

    Upload BIDStatisticsAPI

---
+++ BIDStatisticsAPI/BIDStatisticsAPI/CitiesDataStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BIDStatisticsAPI.Models;
+
+namespace BIDStatisticsAPI
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with ONE."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower."
+                }
+            };
+        }
+    };
+}
---
+++ BIDStatisticsAPI/BIDStatisticsAPI/Controllers/CitiesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIDStatisticsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetCity(int id)
+        {
+            //return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+
+            //find city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+
+        }
+    }
+}
+
